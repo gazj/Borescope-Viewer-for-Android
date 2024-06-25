17525e307af2f852efd400ac763f1bd988767148
gazj <gazzzzzj@gmail.com>
2024-06-25 07:47:16 -0400

    Initial commit

---
+++ app/src/main/java/com/example/borescopeviewer/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.borescopeviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import com.example.borescopeviewer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Borescope Stream Fixer configuration.
+     *
+     * See https://github.com/mkarr/boroscope_stream_fixer for details.
+     */
+    public String input_src = "tcp:10.0.2.2:7060/live/stream"; // -i
+    public String output_dst = "tcp::7060"; // -o
+
+    /**
+     * Load bsf.c as a library.
+     */
+    static { System.loadLibrary("bsf"); }
+
+    /**
+     * Native functions defined in bfs.c and called via JNI.
+     */
+    //public native String bsfStringFromJNI();
+    //public native Integer bsfCallJavaMethod();
+    public native String bsfConnect(String input_src, String output_dst);
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView textview = binding.sampleText;
+        textview.setText("Click 'Connect'");
+
+        // Get button instances.
+        Button connect_btn = (Button) findViewById(R.id.connect_btn);
+        Button disconnect_btn = (Button) findViewById(R.id.disconnect_btn);
+
+        // Define actions for button clicks.
+        connect_btn.setOnClickListener((View.OnClickListener)(new View.OnClickListener() {
+            public final void onClick(View it) {
+                textview.setText(bsfConnect(input_src, output_dst));
+            }
+        }));
+    }
+
+//    public void print_line(String line)
+//    {
+//        Log.d("TEST", line);
+//    }
+}
