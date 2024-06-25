e3931f7b80e981797cf75775596f997e392f4deb
gazj <gazzzzzj@gmail.com>
2024-06-25 11:21:40 -0400

    WIP: Run bsf.c::main in a Java thread

--- app/src/main/java/com/example/borescopeviewer/MainActivity.java
+++ app/src/main/java/com/example/borescopeviewer/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.borescopeviewer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.os.Build;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
-
+import java.util.ArrayList;
+import java.util.List;
 import com.example.borescopeviewer.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +20,11 @@ public class MainActivity extends AppCompatActivity {
      *
      * See https://github.com/mkarr/boroscope_stream_fixer for details.
      */
-    public String input_src = "tcp:10.0.2.2:7060/live/stream"; // -i
-    public String output_dst = "tcp::7060"; // -o
+    public String borescope_input_src = "tcp:192.168.10.123:7060"; // -i
+    public String emulated_input_src = "tcp:10.0.2.2:7060/stream.mjpeg"; // emulation/testing
+    public String output_dst = "/data/output.jmpeg"; // -o
+
+    /*
 
     /**
      * Load bsf.c as a library.
@@ -27,12 +34,32 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Native functions defined in bfs.c and called via JNI.
      */
-    //public native String bsfStringFromJNI();
-    //public native Integer bsfCallJavaMethod();
-    public native String bsfConnect(String input_src, String output_dst);
+    public native int bsfConnect(String input_src, String output_dst);
 
     private ActivityMainBinding binding;
 
+    // Update status message on view. Ensures updates from non-main threads still work.
+    public void setStatusText(String text, String code)
+    {
+        // UI updates cannot occur as a result of JNI calls from other threads.
+        runOnUiThread(new Runnable(){
+            @Override
+            public void run() {
+                TextView status_text = (TextView) findViewById(R.id.status_text);
+                String new_text = code.isEmpty()
+                    ? String.format("%s", text)
+                    : String.format("%s (%s)", text, code);
+                status_text.setText(new_text);
+            }
+        });
+    }
+
+    // Debugging methods, called via JNI.
+    public void log_string(String s) { Log.d("DEBUG", s); }
+    public void log_char(char c) { Log.d("DEBUG", String.format("%c", c)); }
+    public void log_int(int i) { Log.d("DEBUG", String.format("%d", i)); }
+
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,24 +67,32 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
-        TextView textview = binding.sampleText;
-        textview.setText("Click 'Connect'");
+        // Get editable text fields
+        EditText inputSourceField = (EditText) findViewById(R.id.input_source);
+        EditText outputDestinationField = (EditText) findViewById(R.id.output_destination);
 
-        // Get button instances.
+        // Set text field defaults.
+        inputSourceField.setText(emulated_input_src);
+        outputDestinationField.setText(output_dst);
+
+        // Connect button setup.
         Button connect_btn = (Button) findViewById(R.id.connect_btn);
-        Button disconnect_btn = (Button) findViewById(R.id.disconnect_btn);
-
-        // Define actions for button clicks.
         connect_btn.setOnClickListener((View.OnClickListener)(new View.OnClickListener() {
             public final void onClick(View it) {
-                textview.setText(bsfConnect(input_src, output_dst));
+                // Disable the form, requiring an app restart to attempt another connection.
+                inputSourceField.setEnabled(false);
+                outputDestinationField.setEnabled(false);
+                connect_btn.setEnabled(false);
+                // Create a new thread for bsf.c functions to run under.
+                new Thread(){
+                    public void run(){
+                        bsfConnect(
+                            inputSourceField.getText().toString(),
+                            outputDestinationField.getText().toString()
+                        );
+                    }
+                }.start();
             }
         }));
     }
-
-//    public void print_line(String line)
-//    {
-//        Log.d("TEST", line);
-//    }
 }
