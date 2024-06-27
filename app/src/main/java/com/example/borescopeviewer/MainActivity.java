750bc2b6a37bb79394616fd1ebdf1422fa9153ea
gazj <gazzzzzj@gmail.com>
2024-06-27 13:21:11 -0400

    Miscellaneous tweaks, see long msg
    - Set default input address dynamically based on attempted emulation detection
    - Prevent VideoView instance from polling for data until a connection attempt is made
    - Add MediaPlayer debug info logging

--- app/src/main/java/com/example/borescopeviewer/MainActivity.java
+++ app/src/main/java/com/example/borescopeviewer/MainActivity.java
@@ -14,18 +14,12 @@ import android.widget.VideoView;
 
 import com.example.borescopeviewer.databinding.ActivityMainBinding;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
-    /**
-     * Borescope Stream Fixer configuration.
-     *
-     * See https://github.com/mkarr/boroscope_stream_fixer for details.
-     */
-    public String borescope_input_src = "tcp:192.168.10.123:7060"; // -i
-    public String emulated_input_src = "tcp:10.0.2.2:7060/stream.mjpeg"; // emulation/testing
 
     /**
      * Load bsf.c as a library.
@@ -47,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 TextView status_text = (TextView) findViewById(R.id.status_text);
-                String new_text = code.isEmpty()
+                String new_text = code == null || code.isEmpty()
                     ? String.format("%s", text)
                     : String.format("%s (%s)", text, code);
                 status_text.setText(new_text);
@@ -68,31 +62,22 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Set input field defaults.
+        // Set input field defaults, attempting to detect emulated network host address connectivity.
         EditText inputSourceField = (EditText) findViewById(R.id.input_source);
-        inputSourceField.setText(borescope_input_src);
-
-        // Connect button setup.
-        Button connect_btn = (Button) findViewById(R.id.connect_btn);
-        connect_btn.setOnClickListener((View.OnClickListener)(new View.OnClickListener() {
-            public final void onClick(View it) {
-                // Disable the form, requiring an app restart to attempt another connection.
-                inputSourceField.setEnabled(false);
-                connect_btn.setEnabled(false);
-                // Create a new thread for bsf.c functions to run under.
-                new Thread(){
-                    public void run(){
-                        bsfConnect(
-                            inputSourceField.getText().toString(),
-                            "tcp:127.0.0.1:7060"
-                        );
-                    }
-                }.start();
+        inputSourceField.setText("tcp:192.168.10.123:7060");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if(InetAddress.getByName("10.0.2.2").isReachable(3000))
+                        inputSourceField.setText("tcp:10.0.2.2:7060/stream.mjpeg");
+                } catch (IOException e) { /**/ }
             }
-        }));
+        }).start();
 
         // Configure the video player.
         VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setEnabled(false);
 
         // Suppress error dialog when connection is unavailable.
         videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
@@ -101,21 +86,56 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        // Log MediaPlayer info for debugging.
+        videoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+            @Override
+            public boolean onInfo(MediaPlayer mediaPlayer, int what, int extra) {
+                // See https://developer.android.com/reference/android/media/MediaPlayer.OnInfoListener
+                Log.d("DEBUG", String.format("MediaPlayer Info: %d %d", what, extra));
+                return false;
+            }
+        });
 
         // Set a timer for the VideoView to poll the configured output destination.
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
                 try {
+                    if(!videoView.isEnabled()) return;
                     if(!videoView.isPlaying()) {
-                        Uri uri = Uri.parse("tcp://127.0.0.1:7060");
+                        String url = "rtsp://127.0.0.1:7060";
+                        Uri uri = Uri.parse(url);
                         videoView.setVideoURI(uri);
+//                        videoView.setVideoPath(url);
                         videoView.start();
                     } else setStatusText("Streaming...", null);
-                } catch (Exception e) {
-                    //
-                }
+                } catch (Exception e) { /**/ }
             }
         }, 0, 1000);
+
+        // Connect button setup.
+        Button connect_btn = (Button) findViewById(R.id.connect_btn);
+            connect_btn.setOnClickListener((View.OnClickListener)(new View.OnClickListener() {
+            public final void onClick(View it) {
+                // Disable the form, requiring an app restart to attempt another connection.
+                inputSourceField.setEnabled(false);
+                connect_btn.setEnabled(false);
+                // Create a new thread for bsf.c functions to run under.
+                new Thread(){
+                    public void run(){
+                        runOnUiThread(new Runnable(){
+                            @Override
+                            public void run() {
+                                videoView.setEnabled(true);
+                            }
+                        });
+                        bsfConnect(
+                            inputSourceField.getText().toString(),
+                            "tcp:127.0.0.1:7060"
+                        );
+                    }
+                }.start();
+            }
+        }));
     }
 }
